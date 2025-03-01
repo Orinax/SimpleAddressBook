0397b5d339cadc2e985af4452c1bf10164cb4876
Orinax <chaunceypelton@gmail.com>
2025-03-01 15:28:11 +0700

    Completed linkedList.h

--- linkedList.h
+++ linkedList.h
@@ -7,6 +7,7 @@
 
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
+#include <iostream>
 
 // Define the node structure first. (p. 279-280)
 template <class Type>
@@ -61,28 +62,63 @@ template <class Type>
 class linkedListType {
 public:
     // Overload the assignment operator.
-    const linkedListType<Type>& operator=(const linkedListType<Type> &right);
+    const linkedListType<Type>& operator=(const linkedListType<Type> &otherList) {
+        if (this != &otherList) { // This check will avoid self-copying
+            copyList(otherList);
+        }
+        return *this;
+    }
 
     // Initialize the list to an empty state.
-    void initializeList();
+    void initializeList() {
+        destroyList();  // if the list has any nodes, delete them
+    }
 
     // Function to determine whether the list is empty
-    bool isEmptyList() const;
+    bool isEmptyList() const {
+        return (first == NULL);
+    }
 
     // Function to output the data contained in each node.
-    void print() const;
+    void print() const {
+        nodeType<Type> *current = first;  // make a temporary pointer to travel through the list
+
+        current = first;
+        while (current != NULL) {
+            std::cout << current->info << " ";
+            current = current->link;
+        }
+    }
 
     // Function to return the number of nodes in the list.
-    int length() const;
+    int length() const {
+        return count;
+    }
 
     // Function to delete all the nodes from the list.
-    void destroyList();
+    void destroyList() {
+        nodeType<Type> *temp;   // pointer to deallocate the memory occupied by the node
+        while (first != NULL) { // while there are nodes in the list
+            temp = first;       // set temp to the current node
+            first = first->link;// advance first to next node
+            delete temp;        // deallocate the memory occupied by temp.
+        }
+
+        last = NULL;
+        count = 0;
+    }
 
     // Function to return the first element of the list.
-    Type front() const;
+    Type front() const {
+        assert(first != NULL);
+        return first->info;
+    }
 
     // Function to return the last element of the list.
-    Type back() const;
+    Type back() const {
+        assert(last != NULL);
+        return last->info;
+    }
 
     // Function to determine wheather searchItem is in the list.
     virtual bool search(const Type& searchItem) const = 0;
@@ -97,19 +133,34 @@ public:
     virtual void deleteNode(const Type& deleteItem) = 0;
 
     // Function to return an iterator at the beginning of the linked list.
-    linkedListIterator<Type> begin();
+    linkedListIterator<Type> begin() {
+        linkedListIterator<Type> temp(first);
+        return temp;
+    }
 
     // Function to return an iterator one element past the last element of the linked list.
-    linkedListIterator<Type> end();
+    linkedListIterator<Type> end() {
+        linkedListIterator<Type> temp(NULL);
+        return temp;
+    }
 
     // Default constructor
-    linkedListType();
+    linkedListType() {
+        first = NULL;
+        last = NULL;
+        count = 0;
+    }
 
     // Copy constructor
-    linkedListType(const linkedListType<Type> &otherList);
+    linkedListType(const linkedListType<Type> &otherList) {
+        first = NULL;
+        copyList(otherList);
+    }
 
     // Destructor
-    ~linkedListType();
+    ~linkedListType() {
+        destroyList();
+    }
 
 protected:
     int count; // variable to store the number of elements in the list
@@ -119,7 +170,41 @@ protected:
 
 private:
     // Function to make a copy of otherList.
-    void copyList(const linkedListType<Type> &otherList);
+    void copyList(const linkedListType<Type> &otherList) {
+        nodeType<Type> *newNode;  // pointer to create a new node
+        nodeType<Type> *current;  // pointer to traverse the list
+
+        if (first != NULL) {
+            destroyList();
+        }
+
+        if (otherList.first == NULL) {
+            first = NULL;
+            last = NULL;
+            count = 0;
+        }
+        else {
+            current = otherList.first;
+            count = otherList.count;
+
+            // Copy the first node
+            first = new nodeType<Type>;
+            first->info = current->info;
+            first->link = NULL;
+            last = first;
+            current = current->link;
+
+            // Copy the remaining list
+            while (current != NULL) {
+                newNode = new nodeType<Type>;
+                newNode->info = current->info;
+                newNode->link = NULL;
+                last->link = newNode;
+                last = newNode;
+                current = current->link;
+            }
+        }
+    }
 };
 
 
